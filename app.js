088725b450703e2f5536aa1cc916d1570b687c71
Sabelo Mkhabela <xerosandbox@gmail.com>
2025-02-15 16:30:33 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+function clock() {
+    const dt = new Date();
+    let h = dt.getHours();
+    let m = dt.getMinutes();
+    let s = dt.getSeconds();
+
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+    s = s < 10 ? '0' + s : s;
+
+    //date
+    let month = dt.toLocaleString('en', { month: 'long' });
+    let weekDay = dt.toLocaleString('en', { weekday: 'long' });
+    let day = dt.getDay();
+    let year = dt.getFullYear();
+
+    document.getElementById('time').innerText = `${h} : ${m} : ${s}`
+    document.getElementById('date').innerText = `${day} ${month} ${year}` 
+    document.getElementById('day').innerText = `${weekDay}`
+}
+
+
+setInterval(clock, 1000)
